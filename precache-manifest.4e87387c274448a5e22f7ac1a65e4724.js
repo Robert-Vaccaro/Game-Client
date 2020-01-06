d7ef361fa09aab77ace663bffb0447bdc27fc4b6
Bobby <robertvaccaro@Bobbys-MacBook-Pro.local>
2020-01-05 22:34:22 -0800

    Updates

---
+++ precache-manifest.4e87387c274448a5e22f7ac1a65e4724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcd4f262c2696df07448f1df2c643b9",
+    "url": "/Robert-Vaccaro/Game-Client/index.html"
+  },
+  {
+    "revision": "817227528e6fb52733d6",
+    "url": "/Robert-Vaccaro/Game-Client/static/css/main.1fd50fee.chunk.css"
+  },
+  {
+    "revision": "b6101f40bac00bd46663",
+    "url": "/Robert-Vaccaro/Game-Client/static/js/2.129ca569.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/Robert-Vaccaro/Game-Client/static/js/2.129ca569.chunk.js.LICENSE"
+  },
+  {
+    "revision": "817227528e6fb52733d6",
+    "url": "/Robert-Vaccaro/Game-Client/static/js/main.553221af.chunk.js"
+  },
+  {
+    "revision": "7cbd17d1c8ed3d8142c5",
+    "url": "/Robert-Vaccaro/Game-Client/static/js/runtime-main.6ad9b9b1.js"
+  }
+]);
